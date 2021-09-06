c210a96e292a228f43f5b00667e71b12cdb3f231
ArnasStanislovaitis <89936719+ArnasStanislovaitis@users.noreply.github.com>
2021-09-06 12:40:48 +0300

    Revert "Sugadinimass"
    This reverts commit 3c6f23a20bc8be44dc781460491c3f7a47b4d4fb.

--- GitHub mokymai/GitHub mokymai/Program.cs
+++ GitHub mokymai/GitHub mokymai/Program.cs
@@ -5,13 +5,13 @@ namespace GitHub_mokymai
 {
     class Program
     {
-        static void Gain(string[] args)
+        static void Main(string[] args)
         {
 
             List<string> Sarasas = new List<string>() { "gele", "medis", "paukstis", "namas" };
             for (int i = 0; i < Sarasas.Count; i++)
             {
-                Console.WriteLasdfine(Sarasas[i]);d
+                Console.WriteLine(Sarasas[i]);
             }
             
 
