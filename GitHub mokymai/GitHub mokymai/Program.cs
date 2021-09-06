3c6f23a20bc8be44dc781460491c3f7a47b4d4fb
ArnasStanislovaitis <89936719+ArnasStanislovaitis@users.noreply.github.com>
2021-09-06 12:39:04 +0300

    Sugadinimass

--- GitHub mokymai/GitHub mokymai/Program.cs
+++ GitHub mokymai/GitHub mokymai/Program.cs
@@ -5,13 +5,13 @@ namespace GitHub_mokymai
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Gain(string[] args)
         {
 
             List<string> Sarasas = new List<string>() { "gele", "medis", "paukstis", "namas" };
             for (int i = 0; i < Sarasas.Count; i++)
             {
-                Console.WriteLine(Sarasas[i]);
+                Console.WriteLasdfine(Sarasas[i]);d
             }
             
 
