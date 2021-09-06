916cd3b378aa40cfce560564849dc46cdb887fba
ArnasStanislovaitis <89936719+ArnasStanislovaitis@users.noreply.github.com>
2021-09-06 12:26:07 +0300

    Mano pirmas bandymas

---
+++ GitHub mokymai/GitHub mokymai/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitHub_mokymai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> Sarasas = new List<string>() { "gele", "medis", "paukstis", "namas" };
+            for (int i = 0; i < Sarasas.Count; i++)
+            {
+                Console.WriteLine(Sarasas[i]);
+            }
+            
+
+        }
+    }
+}
